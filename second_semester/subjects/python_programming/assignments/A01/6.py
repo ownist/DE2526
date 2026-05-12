0387fa69e4bc2c39f950a4ad4debf4a56e4d9000
ownist <shahedevise@gmail.com>
2026-05-12 22:11:36 +0600

    feat(python): add leap year checker script
    - Implemented logic for leap year detection
    - Included user input for year
    - Added print statements for output

---
+++ second_semester/subjects/python_programming/assignments/A01/6.py
@@ -0,0 +1,9 @@
+# ========================
+# ৬. অধিবর্ষ নির্ণয়
+# ========================
+year = int(input("Enter year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is leap year.")
+else:
+    print(f"{year} is not leap year.")
