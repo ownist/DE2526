e0435ec82cbb172d4f49b4c939ae00d58f5b78df
ownist <shahedevise@gmail.com>
2026-05-12 22:07:25 +0600

    feat(A01): add solution for even/odd checker

---
+++ second_semester/subjects/python_programming/assignments/A01/3.py
@@ -0,0 +1,9 @@
+# =========================
+# ৩. জোড় বা বিজোড় সংখ্যা নির্ণয়
+# =========================
+num = int(input("Enter your number: "))
+
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
